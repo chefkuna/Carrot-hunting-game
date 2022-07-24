118a7459887d7060ad41a751f7d97a30dcff99c4
chefkuna <sukhyun94@naver.com>
2022-07-24 14:37:08 +0900

    JS : game timer

--- main.js
+++ main.js
@@ -1,22 +1,76 @@
-'use strict'
+'use strict';
 const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;
 
 const gameField = document.querySelector('.game__field');
 const fieldRect = gameField.getBoundingClientRect();
+const playBtn = document.querySelector('.play__button');
+const gameTimer = document.querySelector('.game__timer');
+const carrotScore = document.querySelector('.carrot__score');
 
-// const x = Math.floor(Math.random()*(fieldRect.width-CARROT_SIZE));
-// const y = Math.floor(Math.random()*(fieldRect.height-CARROT_SIZE));
+let started = false;
+let score = 0;
+let timer = undefined;
+
+playBtn.addEventListener('click', ()=>{
+  if(started) {
+    stopGame();
+  } else {
+    startGame();
+  }
+  started = !started;
+})
+
+function stopGame() {
+
+}
+
+function startGame() {
+  initGame();
+  showStopButton();
+  showTimerAndCount();
+  startGameTimer();
+}
+
+function showStopButton() {
+  const icon = playBtn.querySelector('.fa-play');
+  icon.classList.add('fa-stop');
+  icon.classList.remove('fa-play');
+}
+
+function startGameTimer () {
+  let remainingTimeSec = GAME_DURATION_SEC;
+  updateTimerText(remainingTimeSec);
+  timer = setInterval(() => {
+    if(remainingTimeSec <= 0) {
+      clearInterval(timer);
+      return;
+    }
+    updateTimerText(--remainingTimeSec);
+  }, 1000);
+}
+
+function updateTimerText(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  gameTimer.innerText = `${minutes}:${seconds}`;
+}
+
+function showTimerAndCount() {
+  gameTimer.style.visibility = 'visible';
+  carrotScore.style.visibility = 'visible';
+}
 
 function initGame() {
-  addItem('carrot', 5, 'img/carrot.png');
-  addItem('bug', 5, 'img/bug.png');
+  gameField.replaceChildren();    // field.innerHTML = '';
+  carrotScore.innerText = CARROT_COUNT;
+  addItem('carrot', CARROT_COUNT, 'img/carrot.png');
+  addItem('bug', BUG_COUNT, 'img/bug.png');
 }
 
 function addItem (className, count, imgPath) {
-  // const x1=0;
-  // const y1=0;
-  // const x2=fieldRect.width;
-  // const y2=fieldRect.height;
   for(let i=0; i<count; i++) {
     const item = document.createElement('img');
     item.setAttribute('class', className);
@@ -24,21 +78,21 @@ function addItem (className, count, imgPath) {
     item.style.position = 'absolute';
     const x = Math.floor(Math.random()*(fieldRect.width-CARROT_SIZE));
     const y = Math.floor(Math.random()*(fieldRect.height-CARROT_SIZE));
-    // const x = randomNumber(x1, x2);
-    // const y = randomNumber(y1, y2);
+
     item.style.left = `${x}px`;
     item.style.top = `${y}px`;
     gameField.appendChild(item);
   }
 }
 
-// function randomNumber(min, max) {
-//   return Math.random() * (max-min) + min;
-// }
+function Timer () {
+  setInterval(() => {
+    
+  }, 1000);
+}
 
-initGame();
+function myTimer() {
 
-const playBtn = document.querySelector('.play__button');
-playBtn.addEventListener('click', ()=>{
-  initGame();
-})
+}
+
+
